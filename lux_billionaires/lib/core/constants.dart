cc109bcfeed965df36c3e75ae9a4cf37017d8571
ismailaboelkher <ismailaboelkhier@gmail.com>
2025-06-03 22:35:25 +0300

    Add lux_billionaires/lib/core/constants.dart 1748979324552

---
+++ lux_billionaires/lib/core/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Define app-wide constants here
+
+const kPrimaryColor = Color(0xFFFFD700); // Gold color for luxury feel
+const kBackgroundColor = Colors.white;
+const kTextColor = Colors.black87;
+
+const kDefaultPadding = 16.0;
